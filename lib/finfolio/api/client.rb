e3cf7a3437874dd62b4bc85e8d306095a01b228c
Cameron Bass <cam@collectiveidea.com>
2018-03-12 12:33:03 -0400

    Add documentation for the endpoints supported.
    This will add clarity for the developers looking to implement this
    gem into their projects.

--- lib/finfolio/api/client.rb
+++ lib/finfolio/api/client.rb
@@ -12,14 +12,22 @@ require "finfolio/api/trading_model"
 require "finfolio/api/cash_value"
 require "finfolio/api/error"
 require "finfolio/version"
-require "pry"
 
 class Finfolio::API::Client
+  # Initializes a new Client object
+  #
+  # @param key      [String]
+  # @param endpoint [String]
+  # @return [Finfolio::API::Client]
   def initialize(key, endpoint)
     @key = key
     @endpoint = URI.parse(endpoint).freeze
   end
 
+  # Get the list of Managers
+  #
+  # @param params [Hash] customize fields
+  # @return [Array<Finfolio::API::Manager>] all the managers within api key's container.
   def managers(params = {})
     response = get("/api/manager", params)
 
@@ -28,6 +36,12 @@ class Finfolio::API::Client
     end
   end
 
+  # Get a single manager from Finfolio
+  #
+  # @param id [String]
+  # @param params [Hash] customize response fields
+  #
+  # @return [Finfolio::API::Manager]
   def manager(id, params = {})
     response = get("/api/manager/#{id}", params)
     response = deconstruct_array(response)
@@ -35,6 +49,12 @@ class Finfolio::API::Client
     Finfolio::API::Manager.new(response)
   end
 
+  # Get a single Account from Finfolio
+  #
+  # @param id [String]
+  # @param params [Hash] customize response fields
+  #
+  # @return [Finfolio::API::Account]
   def account(id, params = {})
     response = get("/api/account/#{id}", params)
     response = deconstruct_array(response)
@@ -42,18 +62,36 @@ class Finfolio::API::Client
     Finfolio::API::Account.new(response)
   end
 
+  # Get the account status of given account
+  #
+  # @param id [String]
+  # @param params [Hash] customize response fields
+  #
+  # @return [Finfolio::API::AccountStatus]
   def account_status(id, params = {})
     response = get("/api/account/status/#{id}", params)
 
     Finfolio::API::AccountStatus.new(response)
   end
 
+  # Get the subtype of a given account
+  #
+  # @param sub_type [String]
+  # @param params [Hash] customize response fields
+  #
+  # @return [Finfolio::API::AccountType]
   def account_type(sub_type, params = {})
     response = get("/api/account/subtype/#{sub_type}", params)
 
     Finfolio::API::AccountType.new(response)
   end
 
+  # Get the account value of a given account
+  #
+  # @param id [String]
+  # @param params [Hash] customize the response fields
+  #
+  # @return [Finfolio::API::AccountValue]
   def account_value(id, params = {})
     response = get("/api/calculation/run/#{id}", params)
     response = deconstruct_array(response)
@@ -61,6 +99,12 @@ class Finfolio::API::Client
     Finfolio::API::AccountValue.new(response)
   end
 
+  # Get the trading model of a given account, use ModelID found on Account
+  #
+  # @param id [String] the account's ModelID
+  # @param params [Hash] customize response fields
+  #
+  # @return [Finfolio::API::TradingModel]
   def trading_model(id, params = {})
     response = get("/api/trading/model/#{id}", params)
     response = deconstruct_array(response)
@@ -68,6 +112,11 @@ class Finfolio::API::Client
     Finfolio::API::TradingModel.new(response)
   end
 
+  # Get a fee schedule of a given account, use FeeScheduleID found on Account
+  #
+  # @param params [Hash] pass the FeeScheduleID within the params
+  #
+  # @return [Finfolio::API::FeeSchedule]
   def fee_schedule(params = {})
     response = get("/api/billing/feeschedule", params)
     response = deconstruct_array(response)
@@ -75,6 +124,12 @@ class Finfolio::API::Client
     Finfolio::API::FeeSchedule.new(response)
   end
 
+  # Get a Cash value of a given account
+  #
+  # @param id [String]
+  # @param params [Hash] customize response fields
+  #
+  # @return [Finfolio::API::CashValue]
   def cash_value(id, params = {})
     response = get("/api/calculation/run/#{id}", params)
     response = deconstruct_array(response)
@@ -82,6 +137,13 @@ class Finfolio::API::Client
     Finfolio::API::CashValue.new(response)
   end
 
+  # Build a custom query from the Finfolio DB
+  #
+  # @param params [Hash]
+  # @param ids [Array] use this to return the accounts from more than one manager
+  #
+  # @return [Finfolio::API::Account]
+  # NOTE: This should be updated to allow different objects to be returned from Finfolio
   def view(params = {}, ids)
     if ids.any?
       params[:filter] = ids.map { |id| "ManagerID='#{id}'" }.join(" OR ")
